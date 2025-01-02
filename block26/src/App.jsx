4ddff2e14b26a59789198f634e76cb6b5296247f
Sharon Marzette <sharonmarzette@Sharons-Air.attlocal.net>
2025-01-02 18:12:10 -0500

    block26 fix

---
+++ block26/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import { useState } from "react";
+import ContactList from "./components/ContactList";
+
+
+export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
+
+  return (
+    <>
+      {selectedContactId ? (
+        <div>Selected Contact View</div>
+      ) : (
+        <ContactList setSelectedContactId={setSelectedContactId} />
+      )}
+    </>
+  );
+}
+
+
+
